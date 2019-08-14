da2b6ca01f1d2117a6e843a632589cd6a7664304
kugi <kugimania@gmail.com>
2019-08-14 14:30:53 +0900

    [update] Chapter 6

--- camera.h
+++ camera.h
@@ -1,8 +1,25 @@
 //
-// Created by 釘田尚弥 on 2019-08-14.
+// Created by kugimasa on 2019-08-14.
 //
 
-#ifndef RAYTRACINGINONEWEEKEND_CAMERA_H
-#define RAYTRACINGINONEWEEKEND_CAMERA_H
+#ifndef CAMERAH
+#define CAMERAH
 
-#endif //RAYTRACINGINONEWEEKEND_CAMERA_H
+#include "ray.h"
+
+class camera {
+public:
+    camera() {
+        lower_left_corner = vec3(-2.0, -1.0, -1.0);
+        horizontal = vec3(4.0, 0.0, 0.0);
+        vertical = vec3(0.0, 2.0, 0.0);
+        origin = vec3(0.0, 0.0, 0.0);
+    }
+    ray get_ray(float u, float v) {return ray(origin, lower_left_corner + u*horizontal + v*vertical - origin);}
+
+    vec3 origin;
+    vec3 lower_left_corner;
+    vec3 horizontal;
+    vec3 vertical;
+};
+#endif //CAMERAH
